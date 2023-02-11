40d13e22949e26994a9d040b04993aae8d6283a8
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 17:36:12 +0300

    fix: address cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var cors = require('cors')
+// var cors = require('cors')
 var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http, {
@@ -82,9 +82,9 @@ io.on('connection', (socket) => {
     });
 });
 
-app.get('/products/:id', cors(), function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for a Single Route'})
-})
+// app.get('/products/:id', cors(), function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for a Single Route'})
+// })
 http.listen(port, function () {
     console.log('CORS-enabled web server listening on port '+ port)
 })
