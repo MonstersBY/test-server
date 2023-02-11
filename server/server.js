e9dd85b904daf92c976e5ab7c5dc434e46ae00ba
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 16:17:37 +0300

    fix: fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-const io = require('socket.io')(http, {
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
+const io = require('socket.io')(3000, {
     cors: {
         origin: ['http://localhost:8080', 'https://admin.socket.io'],
         methods: ['GET', 'POST'],
@@ -80,6 +80,6 @@ io.on('connection', (socket) => {
 });
 
 
-http.listen(port, function() {
-    console.log('listening on *: ' + port);
-});
+// http.listen(port, function() {
+//     console.log('listening on *: ' + port);
+// });
