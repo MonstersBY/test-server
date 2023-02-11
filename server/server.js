8c7a17c8e627136f9b7e1028140a2be6159b260b
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 16:08:53 +0300

    init: new logic

--- server/server.js
+++ server/server.js
@@ -1,26 +1,85 @@
-const {instrument} = require('@socket.io/admin-ui')
-
-const io = require('socket.io')(3000, {
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http, {
     cors: {
-        origin: ['http://localhost:8080', 'https://admin.socket.io']
+        origin: ['http://localhost:8080', 'https://admin.socket.io'],
+        methods: ['GET', 'POST'],
     },
 })
-io.on('connection', socket => {
-    console.log(socket.id);
+const port = process.env.PORT || 3000;
 
-    socket.on('send-message', (message, room)=>{
-        if(room ===''){
-            socket.broadcast.emit('receive-message', message)
-        } else {
-            socket.to(room).emit('receive-message', message)
+let users = []
+let allrooms = []
+
+io.on('connection', (socket) => {
+    // console.log(`User connected ${socket.id}`);
+
+    socket.on('chatMessage', (msg, room) => {
+        const user = users.find(user => user.id === socket.id)
+        io.to(room).emit('message', user.username, msg)
+    })
+    
+    socket.on('join-room', (username, room) => {
+        const user = {
+            username,
+            room,
+            id: socket.id,
         }
-        
-        // io.emit('receive-message', message)
-    })
-    socket.on('join-room', (room, cb) =>{
-        socket.join(room)
-        cb(`Joined ${room}`)
-    })
-})
+        users.push(user)
+        if(user.room){
+            const rooms = {
+                room,
+                count: 0,
+            }
+            const id = allrooms.findIndex(name => name.room === room)
+            if (id == -1) {
+                // rooms.count++
+                console.log(rooms);
+                allrooms.push(rooms)
+                console.log(allrooms);
+            } else {
+                // allrooms[id].count++
+            }
+            console.log('room: '+ room);
+            socket.join(room)
+            socket.emit('create-room', room)
+            io.to(room).emit('all-user-room', users)
+        }
 
-instrument(io, {auth: false})
+    })
+    io.emit('room-list', allrooms)
+
+    // console.log(io.sockets.adapter.rooms);
+
+    // console.log(socket.rooms);
+
+    // console.log(Object.keys(io.engine.clients)) //all users
+    
+
+    // console.log(socket.rooms);
+
+    socket.on('disconnect', () => {
+        const index = users.findIndex(user => user.id === socket.id)
+        if (index !== -1) {
+            // for (let obj of allrooms) {
+            //     if (obj.room === users[index].room) {
+            //         obj.count--
+            //         break
+            //     }
+            // }
+            // const delRoom = allrooms.findIndex(room => room.count === 0)
+            // if (delRoom !== -1) {
+            //     allrooms.splice(index, 1);
+            // }
+            users.splice(index, 1);
+        }
+        console.log(allrooms);
+        // console.log(`Socket disconnect! ${socket.id}`);
+    });
+});
+
+
+http.listen(port, function() {
+    console.log('listening on *: ' + port);
+});
