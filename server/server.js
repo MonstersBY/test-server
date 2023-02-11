c571c44fa20df6b581ff4351b04ed2bea367b135
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 18:06:58 +0300

    fix:normal

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var cors = require('cors')
+// var cors = require('cors')
 var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http, {
@@ -86,10 +86,10 @@ io.on('connection', (socket) => {
     });
 });
 
-app.get('/products/:id', cors(corsOptions), function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for a Single Route'})
-})
-http.listen(3000, function () {
+// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for a Single Route'})
+// })
+http.listen(port, function () {
     console.log('CORS-enabled web server listening on port '+ port)
 })
 // http.listen(port, function() {
