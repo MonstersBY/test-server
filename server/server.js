7dcaba7cfee2a218a0903bea6bc25c17a608a7ad
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 17:19:37 +0300

    fix: add CORS

--- server/server.js
+++ server/server.js
@@ -85,7 +85,7 @@ io.on('connection', (socket) => {
 app.get('/products/:id', cors(), function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for a Single Route'})
 })
-app.listen(80, function () {
+app.listen(port, function () {
     console.log('CORS-enabled web server listening on port 80')
 })
 // http.listen(port, function() {
